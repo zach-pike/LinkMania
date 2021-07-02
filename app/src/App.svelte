0170b9525cf2374b74b73f2ad53562648701159b
Zachary Pike <ZachTurnPike@gmail.com>
2021-07-01 23:47:32 -0400

    added some baisic styling

--- app/src/App.svelte
+++ app/src/App.svelte
@@ -1,6 +1,21 @@
+<svelte:head>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
+</svelte:head>
+
 <script lang="ts">
 	import { Router, Route } from "svelte-navigator"
+	import { 
+		Navbar,
+		NavbarBrand,
+		NavItem
+	} from "sveltestrap"
 </script>
+
+<Navbar color="dark">
+	<NavbarBrand>
+		<span class="LinkManiaLogoLink">Link</span>Mainia
+	</NavbarBrand>
+</Navbar>
 
 <Router>
 	<Route path="/">
@@ -10,3 +25,10 @@
 		<p>data: uid {params.user}</p>
 	</Route>
 </Router>
+
+<style>
+	.LinkManiaLogoLink {
+		text-decoration: underline;
+		color: #0000EE;
+	}
+</style>
