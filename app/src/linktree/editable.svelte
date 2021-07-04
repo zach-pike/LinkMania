42e35631e5ab1db95bac43bd7a3968dcd681acb3
Zachary Pike <ZachTurnPike@gmail.com>
2021-07-04 12:15:27 -0400

    added editing links and adding links

--- app/src/linktree/editable.svelte
+++ app/src/linktree/editable.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { Toast, ToastBody, ToastHeader, Input, Label, Button } from "sveltestrap"
+    import { Toast, ToastBody, ToastHeader, Input, Label, Button, Modal, ModalHeader, ModalBody, ModalFooter } from "sveltestrap"
     import Listitem from "./listitem.svelte";
 
     import { onMount } from "svelte";
@@ -8,8 +8,8 @@
 
     //firebase stuff
     import { db } from "../firebase"
-    import { collectionData } from "rxfire/firestore";
-    import { startWith } from "rxjs/operators";
+    import { collectionData, doc } from "rxfire/firestore";
+    import { dematerialize, startWith } from "rxjs/operators";
     import type { Observable } from "rxjs";
 
     let query = db.collection("users").doc(uid).collection("links").orderBy("created")
@@ -28,13 +28,62 @@
         links.subscribe((data) => console.log(data))
     })
 
+    let editText: string
+    let editHref: string
+
+    //text input binds
+    let addText: string
+    let addHref: string
+
     function remove(id: string) {
         db.collection("users").doc(uid).collection("links").doc(id).delete()
     }
 
+    let activeEditID = ""
+
+    //edit functions
+    async function edit(id: string) {
+        open = true;
+        activeEditID = id
+
+        let doc = await db.collection("users").doc(uid).collection("links").doc(id).get()
+
+        editText = doc.data().linkText
+        editHref = doc.data().linkHref
+    }
+
+    function setEdit() {
+        open = false;
+        db.collection("users").doc(uid).collection("links").doc(activeEditID).set({ linkText: editText, linkHref: editHref, created: Date.now() })
+    }
+
+    //add a link to the db
+    function AddLink() {
+        db.collection("users").doc(uid).collection("links").add({ linkText: addText, linkHref: addHref, created: Date.now() })
+        addHref = ""
+        addHref = ""
+    }
+
+    let open = false;
+    const toggle = () => (open = !open);
 </script>
 
 <h4 class="text-center mt-4">Add a Link</h4>
+
+<Modal isOpen={open} {toggle}>
+    <ModalHeader {toggle}>Edit Link</ModalHeader>
+    <ModalBody>
+            <Label for="EditLinkName">Link Title</Label>
+            <Input type="text" id="EditLinkName" bind:value="{editText}" />
+    
+            <Label for="EditLinkPath">Link path</Label>
+            <Input type="text" id="EditLinkPath" bind:value="{editHref}" />
+    </ModalBody>
+    <ModalFooter>
+      <Button color="primary" on:click={setEdit}>Set</Button>
+      <Button color="secondary" on:click={toggle}>Cancel</Button>
+    </ModalFooter>
+</Modal>
 
 <div class="d-flex justify-content-center">
     <Toast>
@@ -43,12 +92,12 @@
         </ToastHeader>
         <ToastBody>
             <Label for="LinkName">Link Title</Label>
-            <Input type="text" id="LinkName" />
+            <Input type="text" id="LinkName" bind:value="{addText}" />
     
             <Label for="LinkPath">Link path</Label>
-            <Input type="text" id="LinkPath" />
+            <Input type="text" id="LinkPath" bind:value="{addHref}" />
             
-            <Button outline color="primary" class="mt-2">Add</Button>
+            <Button outline color="primary" class="mt-2" on:click="{AddLink}">Add</Button>
         </ToastBody>
     </Toast>
     
@@ -59,7 +108,13 @@
 <div class="d-flex mt-4 justify-content-center">
     <div class="LinkList">
         {#each $links as link}
-            <Listitem isLinkOwner linkText="{link.linkText}" linkHref="{link.linkHref}" id="{link.id}" on:delete="{ (data) => remove(data.detail.id) }" />
+            <Listitem isLinkOwner 
+                      linkText="{link.linkText}" 
+                      linkHref="{link.linkHref}" 
+                      id="{link.id}" 
+                      on:delete="{ (data) => remove(data.detail.id) }"
+                      on:edit="{ (data) => edit(data.detail.id) }" 
+            />
         {/each}
     </div>
 </div>
--- todo.md
+++ todo.md
@@ -1,4 +1,2 @@
 # TODO
- - make it so you can add links
- - make it so you can edit links
  - make list viewable to people
